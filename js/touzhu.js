f5d9710d085c90daddf2ac8d9655d7b9724d42fa
yanxing <1103157770@qq.com>
2018-04-24 20:02:00 +0800

    修改一些样式

--- js/touzhu.js
+++ js/touzhu.js
@@ -51,17 +51,51 @@ $(function(){
 		// 投注按钮点击事件(如果有选中筹码，则else 若无则提示输入下注金额)
 		if(!getChouma()){
 			layer.prompt({'title':'请输入投注金额'},function(money,index){
+				//如果没有选中筹码，则提示输入金额
 				layer.close(index);
-				layer.confirm('确认您的下注信息',{btn:['下注','取消']},function(touzhunum,index){
-					layer.msg('下注成功'+getTouzhu());
+				var cont = '您的投注金额为：'+money+'元。'+'下注号码为：'+getTouzhu();
+				layer.confirm(cont,{btn:['下注','取消']},function(touzhunum,index){
+					//判断用户点击按钮，此处应该写ajax请求发送下注数据
+					layer.msg('下注成功');
+					$.ajax({
+            			url:"#",
+            			data:{'money':money,'userid':userid,'haoma':getTouzhu(),'qihao':qihao},
+            			type:"Post",
+            			dataType:"json",
+            			success:function(data){
+               			layer.msg(data.msg);
+            			},
+            			error:function(data){
+                		$.messager.alert('错误',data.msg);
+            		}
+        			});
 				},function(){
 					layer.msg('已取消下注');
 				});
 		});
 		} else {
-			layer.msg('下注成功'+getTouzhu());
+			var cont = '您的投注金额为：'+getChouma()+'元。'+'下注号码为：'+getTouzhu();
+			layer.confirm(cont,{btn:['下注','取消']},function(){
+				//此处写下注请求AJAX
+				layer.msg('下注成功');
+				$.ajax({
+            			url:"#",
+            			data:{'money':money,'userid':userid,'haoma':getTouzhu(),'qihao':qihao},
+            			type:"Post",
+            			dataType:"json",
+            			success:function(data){
+               			layer.msg(data.msg);
+            			},
+            			error:function(data){
+                		$.messager.alert('错误',data.msg);
+            		}
+        			});
+			},function(){
+				layer.msg('已取消下注');
+			});
+			
 		}
 		
-		console.log(getChouma());
+		// console.log(getChouma());
 	});
 });
