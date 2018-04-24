7ff9e23841b71abc94eb28fec9c7849a8d253ae5
yanxing <1103157770@qq.com>
2018-04-24 10:05:46 +0800

    第一次写入JS文件

---
+++ js/function.js
@@ -0,0 +1,51 @@
+// 查看当前筹码是否被选中
+function choumaCurrent($this) {
+	return $this.hasClass('choumacurrent');
+}
+// 查看当前图案是否被选中
+function gameimgCurrent($this) {
+	return $this.hasClass('gameimgcurrent');
+}
+// 获取当前选中的投注图案数量
+function gameimgnum() {
+	var num = 0;
+	var divs = $('.gamecontrol > div');
+	divs.each(function(index,val){
+		if($(val).hasClass('gameimgcurrent')){
+			num++;
+		}
+	});
+	return num;
+}
+// 获取当前玩法
+function getGameMethod() {
+	return $('#wanfa').val();
+}
+// 设置当前玩法CSS类
+function setGameMethod() {
+	if($('#wanfa').val() == 2){
+		$('#gameTwo').addClass('gamecurrent');
+	} else if($('#wanfa').val() == 3) {
+		$('#gameThree').addClass('gamecurrent');
+	}
+}
+// 获取当前选中筹码值
+function getChouma() {
+	var s = $('.choumaimg');
+	var money = 0;
+	s.each(function(i,v){
+		if($(v).hasClass('choumacurrent')) {
+			money = $(this).attr('money');
+		
+		}
+	});
+		return money;
+}
+// 获取选中的投注图案信息
+function getTouZhu() {
+	var a = $('.gamecontrol > div');
+	var zhu = '';
+	a.each(function(i,v){
+
+	});
+}
---
+++ js/touzhu.js
@@ -0,0 +1,64 @@
+$(function(){
+	setGameMethod();//初始化玩法
+	$('.gamecontrol > div ').on('click',function(){
+		//给投注图案添加点击事件
+		if(gameimgCurrent($(this))){
+			$(this).removeClass('gameimgcurrent');
+		} else {
+			if(getGameMethod() == 2){
+				if(gameimgnum() < 2){
+					$(this).addClass('gameimgcurrent');
+				} else {
+					layer.msg('当前为两骰玩法，最多下注两个图案！',{time:1300});
+				}
+			} else if(getGameMethod() == 3) {
+				if(gameimgnum() < 3) {
+					$(this).addClass('gameimgcurrent');
+				} else {
+					layer.msg('当前为三骰玩法，最多下注三个图案！',{time:1300});
+				}
+			}
+			// $(this).siblings().removeClass('gameimgcurrent');
+		}
+		// console.log(gameimgnum());
+		console.log(getGameMethod()+'玩法');
+});
+	$('.zaixian').on('mouseover',function(){
+		// 给在线人数框添加滚动类
+		$(this).addClass('scroll');
+});
+	$('.zaixian').on('mouseout',function(){
+		//去除在线人数框滚动效果
+		$(this).removeClass('scroll');
+});
+	$('.choumaimg').on('click',function(){
+		// 给筹码框添加选中状态
+		if(choumaCurrent($(this))){
+			$(this).removeClass('choumacurrent');
+		} else {
+			$(this).addClass('choumacurrent');
+			$(this).siblings().removeClass('choumacurrent');
+		}
+	});
+	$('#xiazhubtn').on('click',function(){
+		if(!gameimgnum()){
+			layer.msg('请选择图案',{time:1300});
+			return;
+		}
+		// 投注按钮点击事件(如果有选中筹码，则else 若无则提示输入下注金额)
+		if(!getChouma()){
+			layer.prompt({'title':'请输入投注金额'},function(money,index){
+				layer.close(index);
+				layer.confirm('确认您的下注信息',{btn:['下注','取消']},function(touzhunum,index){
+					layer.msg('下注成功');
+				},function(){
+					layer.msg('已取消下注');
+				});
+		});
+		} else {
+
+		}
+		
+		console.log(getChouma());
+	});
+});
